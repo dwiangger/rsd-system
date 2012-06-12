0c86ad87df600f81003b2ade320ca4124bf77f1e
heosuax <heosuax@gmail.com>
2012-06-12 17:06:33 +0700

    Removing Blueprint. Using Bootstrap.

--- rsd-system/application/views/template/template.php
+++ rsd-system/application/views/template/template.php
@@ -2,12 +2,20 @@
 <head>
 	<title>Project Tracking System</title>
 	<!-- Default js library & css -->
-	<link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/style.css" type="text/css">
-	<link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/jquery-ui.css" type="text/css">
+	<!-- <link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/style.css" type="text/css">
+	<link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/jquery-ui.css" type="text/css"> -->
+	
+	<!-- Blueprint -->
+	<!-- 
 	<link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/blueprint/screen.css" type="text/css">
 	<link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/blueprint/plugins/fancy-type/screen.css" type="text/css">
-	
-	<script type="text/javascript" src="<?= $this->config->item('base_url') ?>scripts/jquery.js"></script>
+	 -->
+	 
+	 <!-- Bootstrap -->
+	 <link rel="stylesheet" href="<?= $this->config->item('base_url') ?>css/bootstrap.css" type="text/css">
+	 
+	 <script type="text/javascript" src="<?= $this->config->item('base_url') ?>scripts/jquery.js"></script>
+	 <script type="text/javascript" src="<?= $this->config->item('base_url') ?>scripts/bootstrap.js"></script>
 	<script type="text/javascript" src="<?= $this->config->item('base_url') ?>scripts/jquery-ui.js"></script>
 	<!-- custom js & css -->
 	<?= $_scripts ?>
@@ -15,38 +23,58 @@
 
 </head>
 <body>
-	<div id="header" class="container">
-<div class="logo span-6">
-	<a href="<?= $this->config->item('base_url') ?>" title="Home">
-		<img src="<?= $this->config->item('base_url') ?>images/gcs_logo.jpg" alt="GCS"/>
-	</a>
-</div>
-<div class="title span-15 ">
-	<div class="name">RSD PROJECT MANAGEMENT SYSTEM</div>
-	<div class="slogan">TS Division - Not just test, build your best !</div>
-</div>
-<div class="user-info span-3 last">
-	<div>Welcome <a href="<?php
-		$userInfo = $this->authentication->getUserId();
-		echo $userInfo?$userInfo:'#'; 
-	?>"><?= $userInfo?$userInfo:'Guest'; ?></a></div>
-	<div><?php echo date("d M, Y")?></div>
-</div>
-	</div>
-	<div id="navigator" class="container">
+<!-- fixed Header -->
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">RSD system</a>
+          <div class="btn-group pull-right">
+            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+              <i class="icon-user"></i> <?php
+              	$userInfo = $this->authentication->getUserId(); 
+				echo $userInfo?$userInfo:'Guest'; ?>
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="<?= $userInfo?$userInfo:'#'; ?>">Profile</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Sign Out</a></li>
+            </ul>
+          </div>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+<!-- end of Header -->
+
+<!-- container -->
+<div class="container">
+	<div id="navigator">
 <?= $_navigation ?>
 	</div>
-	<div id="main-content" class="container">
+	<div id="main-content">
 <?= $_content ?>
 	</div>
 	<hr />
-	<div id="footer" class="container">
-<div class="copyright span-12">&copy; 2012. RSD system.</div>
-<div class="footer-link span-12 last">
+	<div class="footer">
+<div class="copyright">&copy; 2012. RSD system.</div>
+<div class="footer-link">
 	<a href="#">About</a> - 
 	<a href="#">Help</a> - 
 	<a href="mailto:anntvo@gcs-vn.com">Contact</a>
 </div>
 	</div>
+</div> <!-- close:container -->
 </body>
 </html>
