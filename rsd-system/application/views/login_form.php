10d68376fed89ec2ea678f8f6a2fe7fa16f49754
heosuax <heosuax@gmail.com>
2012-06-14 17:29:02 +0700

    Apply bootstrap to login form

--- rsd-system/application/views/login_form.php
+++ rsd-system/application/views/login_form.php
@@ -1,45 +1,65 @@
-	<?php 
-	if ( $displayAlert ) {
-		if ( isset($delay) ) {
-			?>
-			<div>You are logged failed <?php echo $numOfFailed; ?> time(s). Please wait and try again after <?php echo date("i:s",$numOfFailed*60-$delay); ?>s.</div>
-			<?php 
-		}else 
-		{
-			?>
-			<div>Invalid user name or password. Please try again.</div>
-			<?php 
-		}
-	}
-	if ( $displayForm ) {
-		$attributes = array('class' => 'form-horizontal');
-		?>
-	<div class="span4 offset2">
-	<div class="row">
-		<div class="span4" style="text-align: center;">
-			<h3>Log in :</h3>
+<?php 
+/**
+ * $delay: 			If reached over 3 failed times, prevent login. 
+ * $displayAlert: 	Display or not alerting abot wrong username/password 
+ * None of 2 above is set ? go ahead. 
+ */
+?>
+<div class="row">
+	<div class="span4 offset4" style="background:url('../../img/lock.png') no-repeat;">
+	<?php echo form_open('authenticate/login'); ?>
+				<?php 
+					if ( $displayAlert ) 
+					{
+						?>
+		<div class="row">
+			<div class="span3 offset1">
+				<h4>Invalid user name/password :</h4>
+			</div>
 		</div>
-	</div>
-	<?php echo form_open('authenticate/login',$attributes); ?>
-<fieldset>
-	<div class="control-group">
-		<label class="control-label" for="input01">Username :</label>
-		<div class="controls">
-		<input type="text" class="input-large" name="user-name" id="user-name" value="" />
+						<?php 
+					}else
+					{
+						?>
+		<div class="row">
+			<div class="span2 offset2">
+				<h4>Login :</h4>
+			</div>
 		</div>
-	</div>
-	<div class="control-group">
-		<label class="control-label" for="input01">Pasword :</label>
-		<div class="controls">
-		<input type="password" class="input-large" name="password" id="password" value="" />
+						<?php 
+					}
+				?>
+<fieldset class="control-group<?= $displayAlert?" error":"" ?>">
+		<div class="row">
+			<div class="span3 offset1">
+<input type="text" name="user-name" id="user-name" value="" placeholder="username" <?= isset($delay)?"disabled=\"disabled\"":"" ?> />
+<input type="password" name="password" id="password" value="" placeholder="password" <?= isset($delay)?"disabled=\"disabled\"":"" ?> />
+
+			</div>
 		</div>
-	</div>
-	<div class="control-group" style="text-align: right;">
-		<input type="submit" class="btn btn-primary" name="submit" id="submit" value="Log in" />
-	</div>
 </fieldset>
-	<?php echo form_close(); ?>
+<?php 
+	if ( isset($delay) )
+	{
+		?>
+			<div class="row">
+				<div class="span3 offset1" style="text-align:right;">
+					<div class="small-attention">You are logged failed <?php echo $numOfFailed; ?> time(s).</div>
+					<div class="small-attention">Please try again after <?php echo date("i:s",$numOfFailed*60-$delay); ?>s.</div>
+				</div>
+			</div>
+		<?php 
+	}else
+	{
+		?>
+	<div class="row">
+		<div class="span4" style="text-align:right;">
+<input type="submit" class="btn btn-primary" name="submit" id="submit" value="Log in" />
+		</div>
 	</div>
 		<?php 
 	}
-	?>
+?>
+	<?php echo form_close(); ?>
+	</div>
+</div>
