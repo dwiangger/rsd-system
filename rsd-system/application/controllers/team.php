aa2bf1dae4e181259c70b90f7238b868250c5b9d
Thien An <heosuax@gmail.com>
2012-06-28 22:31:11 +0700

    Implement team CRUD

--- rsd-system/application/controllers/team.php
+++ rsd-system/application/controllers/team.php
@@ -7,6 +7,55 @@ class Team extends CI_Controller {
 		//For dev debug
 		//$this->output->enable_profiler(TRUE);
 		$this->load->library('auth/authentication');
+				$this->load->library('crudlib/crud');
+		
+		$this->crud->TableName("teams");
+		
+		$this->crud->Link("view_list",
+			$this->config->item('base_url')."index.php/team/view_list/{page-index}");
+		$this->crud->Link("view_edit",
+			$this->config->item('base_url')."index.php/team/view_edit/{item-index}");
+		$this->crud->Link("view_confirmDelete",
+			$this->config->item('base_url')."index.php/team/view_confirm/{item-index}");
+		$this->crud->Option('itemName','Team');
+			
+		$this->crud->ColumnDefine(array(
+			'id' => array(
+				'display' => FALSE,
+				'primary' => TRUE
+			),
+			'name' => array(
+				'display' => TRUE, 
+				'header' => "Name",
+				'width' => 240
+			),
+			'description'=>array(
+				'display' => TRUE,
+				'header' => "Description",
+				'inputType' => 'textarea',
+				'class' => 'span5',
+				'inputData' => array(
+					'value' => 'team desc',
+					'description' => 'Team description'
+				)
+			),
+			'project_id'=>array(
+				'display' => true,
+				'header' => 'Project',
+				'ref' => array(
+					'displayCol' => 'name',
+					'firstChain' => 'projects',
+					'lastChain' => 'projects',
+					'chain' => array(
+						'projects' => array(
+							'indexCol' => 'id',
+							'refCol' => NULL, 
+							'nextChain' => NULL
+						)
+					)
+				)
+			)
+		));
 	}
 	public function index($teamId = 0)
 	{
@@ -49,19 +98,142 @@ class Team extends CI_Controller {
 			'_content','team_view',$data);
 		$this->template->render();
 	}
+	public function view_list($page = 1)
+	{
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->PageIndex($page);
+				$this->crud->Link("view_detail",
+			$this->config->item('base_url')."index.php/team/view_detail/{item-index}");
+		
+		$data = $this->crud->render_list();
+		
+		/* Result */
+		$this->template->write_view(
+			'_navigation',
+			'template/navigation',array());
+		$this->template->write(
+			'_content',
+			$data);
+		$this->template->render();
+	}
+	
+	public function view_detail($id = 0)
+	{
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->ItemId($id);
+		$data = $this->crud->render_detail();
+		
+		if ($data == NULL)
+		{
+			show_404($this->uri->uri_string(),FALSE);
+		}
+		/* Result */
+		$this->template->write_view(
+			'_navigation',
+			'template/navigation',array());
+		$this->template->write(
+			'_content',
+			$data);
+		$this->template->render();
+	}
 	/**
 	 * Display update form 
 	 */
-	public function edit()
+	public function view_edit($id = 0)
 	{
-		$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->ItemId($id);
+		$this->crud->Link('update',
+			site_url('/team/update/{item-index}')
+		);
+		$data = $this->crud->render_editForm();
+		
+		if ($data == NULL)
+		{
+			show_404($this->uri->uri_string(),FALSE);
+		}
+		/* Result */
+		$this->template->write_view(
+			'_navigation',
+			'template/navigation',array());
+		$this->template->write(
+			'_content',
+			$data);
+		$this->template->render();
+	}
+	
+	public function view_confirm($id = 0)
+	{
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->ItemId($id);
+		$this->crud->Link('delete',
+			site_url('/team/delete/{item-index}')
+		);		
+		$data = $this->crud->render_confirmDelete();
+		
+		if ($data == NULL)
+		{
+			show_404($this->uri->uri_string(),FALSE);
+		}
+		/* Result */
+		$this->template->write_view(
+			'_navigation',
+			'template/navigation',array());
+		$this->template->write(
+			'_content',
+			$data);
+		$this->template->render();
+	}
+	
+	public function view_create()
+	{
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->Link('create',
+			site_url('/team/create')
+		);
+		$data = $this->crud->render_createForm();
+
+		/* Result */
+		$this->template->write_view(
+			'_navigation',
+			'template/navigation',array());
+		$this->template->write(
+			'_content',
+			$data);
+		$this->template->render();
+	}
+	/**
+	 * Perform create
+	 */
+	public function create()
+	{
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$id = $this->crud->action_create();
+
+		redirect('/team/view_detail/'.$id, 'refresh');
 	}
 	/**
 	 * Perform updating
 	 */
-	public function update()
+	public function update($id = 0)
 	{
-		$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->ItemId($id);
+		$this->crud->action_update();
+
+		redirect('/team/view_detail/'.$id, 'refresh');
+
+	}
+	/**
+	 * Perform delete
+	 */
+	public function delete($id = 0)
+	{
+		//$this->authentication->checkLogin(TRUE,NULL,'authenticate/login');
+		$this->crud->ItemId($id);
+		$this->crud->action_delete();
+		
+		redirect('/team/view_list/', 'refresh');
 	}
 }
 ?>
